5f4b09e4b141cd912f4fea097515d5b42448f906
becka777 <157316467+becka777@users.noreply.github.com>
2024-10-09 13:43:05 +0200

    Finalizado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [input, setInput] = useState ('')
+
+  const alhacerclick = (value) =>{
+      setInput ( input + value)
+  }
+
+  const botonborrar = () => {
+    setInput('')
+  }
+
+  const botonCalcular = () =>{
+    try{
+      const result = eval(input);
+      setInput(result.toString());
+    } catch (error) {
+      setInput('Error')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+    <input type="text" value={input} readOnly />
+    <div className="button-container">
+      <button onClick={() => alhacerclick('1')}>1</button>
+      <button onClick={() => alhacerclick('2')}>2</button>
+      <button onClick={() => alhacerclick('3')}>3</button>
+      <button onClick={() => alhacerclick('+')}>+</button>
+      <button onClick={() => alhacerclick('4')}>4</button>
+      <button onClick={() => alhacerclick('5')}>5</button>
+      <button onClick={() => alhacerclick('6')}>6</button>
+      <button onClick={() => alhacerclick('-')}>-</button>
+      <button onClick={() => alhacerclick('7')}>7</button>
+      <button onClick={() => alhacerclick('8')}>8</button>
+      <button onClick={() => alhacerclick('9')}>9</button>
+      <button onClick={() => alhacerclick('*')}>*</button>
+      <button onClick={() => alhacerclick('0')}>0</button>
+      <button onClick={() => alhacerclick('.')}>,</button>
+      <button onClick={botonCalcular}>=</button>
+      <button onClick={() => alhacerclick('/')}>/</button>
+      <button onClick={botonborrar}>C</button>
     </div>
+  </div>
   );
 }
 
